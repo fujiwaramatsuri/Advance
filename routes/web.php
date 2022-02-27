6e5b441435805b538d8f213f4ec7942cfe3861e1
fujiwaramatsuri <92214720+fujiwaramatsuri@users.noreply.github.com>
2022-02-27 19:58:46 +0900

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdvanceController;
 
+// お問い合わせ入力ページ
+Route::get('/', [AdvanceController::class,'input']);
+
+// 確認ページ
+Route::get('/Advance/confirm', [AdvanceController::class,'confirm']);
+
+
+
+// 完了ページ
+Route::get('/complete', [AdvanceController::class,';complete']);
+
+//検索結果を表示する
+// Route::get('/serch',[AdvanceController::class,';serch']);
+
+//ユーザー一覧と検索画面
+// Route::get('/',[AdvanceController::class,';index']);
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +30,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+Route::get('/complete', function () {
+    return view('search');
 });
